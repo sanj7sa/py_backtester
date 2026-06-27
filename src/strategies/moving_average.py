a4e95e2dac53d38f041063f2136b8f4550c9aa6a
sanj7sa <sanjaysaravan2018@gmail.com>
2026-06-28 00:42:36 +0100

    Finished the moving averages strategy

--- src/strategies/moving_average.py
+++ src/strategies/moving_average.py
@@ -9,4 +9,29 @@ class MovingAverageCrossStrategy(BaseStrategy):
     It buys when a fast moving average crosses above a slow moving average.
     """
     def __init__(self, short_window=50, long_window=200, name="MA Crossover"):
+        # Gets the parent strategy to be executed first
         super().__init__(name)
+
+        # Defines the parameters for our strategy
+        self.short_window = short_window
+        self.long_window = long_window
+    
+    def generate_signals(self, data: pd.DataFrame) -> pd.DataFrame:
+        """
+        Takes the raw market data and calculates 2 moving averages and outputs when you have to buy and sell.
+        """
+        # Creates a copy to avoid affecting original data
+        df = data.copy()
+
+        # Calculates the short and long simple moving averages
+        df['short_ma'] = df['close'].rolling(window=self.short_window).mean()
+        df['long_ma'] = df['close'].rolling(window=self.long_window).mean()
+
+        # Assume at the start that we do nothing
+        df['signal'] = 0
+        
+        # If the short MA is larger than long then momentum is up so buy (1) and the opposite means sell (-1)
+        df.loc[df['short_ma'] > df['long_ma'], 'signal'] = 1
+        df.loc[df['short_ma'] < df['long_ma'], 'signal'] = -1
+
+        return df
